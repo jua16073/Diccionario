e51aca03f7d8e5d01d7569f1a4d56a6599a9af21
Rodrigo Juárez <Rodrigo Jurez>
2017-03-23 11:26:00 -0600

    ...

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,11 @@
-
+import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado= new Scanner(System.in);
+		System.out.println("Ingrese el texto a traducir");
+		String text= teclado.nextLine();
 
 	}
 

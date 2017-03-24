b3a3e40b4bf2ab991b261433b569231edcb3a75e
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2017-03-23 19:05:58 -0600

    s

--- src/Traducir.java
+++ src/Traducir.java
@@ -1,6 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.lang.Object;
 import java.util.TreeSet;
 public class Traducir {
+	BufferedReader in = new BufferedReader(new FileReader("<diccionario.txt>"));
 	
-
 }

a355562f9a53d84850979a2ac3d29a6a6c141efa
Rodrigo Juárez <Rodrigo Jurez>
2017-03-23 19:32:55 -0600

    jalar archivo

--- src/Principal.java
+++ src/Principal.java
@@ -1,12 +1,15 @@
+import java.io.IOException;
 import java.util.Scanner;
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Scanner teclado= new Scanner(System.in);
+		Traducir trad= new Traducir();
+		String y= trad.lectura();
+		System.out.println(y);
 		System.out.println("Ingrese el texto a traducir");
 		String text= teclado.nextLine();
-
 	}
 
 }
--- src/Traducir.java
+++ src/Traducir.java
@@ -1,8 +1,23 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.lang.Object;
 import java.util.TreeSet;
+
+import javax.swing.JFileChooser;
 public class Traducir {
-	BufferedReader in = new BufferedReader(new FileReader("<diccionario.txt>"));
+String lectura() throws IOException{
+		String a = null; 
+		
+		JFileChooser archivo = new JFileChooser();
+		archivo.showOpenDialog(null);
+		BufferedReader reader = new BufferedReader(new FileReader(archivo.getSelectedFile().getPath()));
+		
+		while(reader.ready()){
+			a = reader.readLine();
+			return a;
+		}
+		return a;
+	}
 	
 }
